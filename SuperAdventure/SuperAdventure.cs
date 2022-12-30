81688216aa60686653cc8fadd15350fee496495d
ianpm <ianpm@DESKTOP-24NDPQL>
2022-12-30 12:15:19 -0500

    Add project files.

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,15 @@
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        public SuperAdventure()
+        {
+            InitializeComponent();
+        }
+
+        private void SuperAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
